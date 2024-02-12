bffca481870b659e6f8289128d238c0ac889ebfe
Aleksandr Ufimtsev <fimak@bk.ru>
2024-02-13 01:45:36 +0700

    feat: handle 500 error

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,16 @@ import usersAPI from './api/users';
 const server = createServer((req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
   const { url } = req;
 
-  if (url && url.startsWith('/api/users')) {
-    usersAPI(req, res);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Non existing endpoint' }));
+  try {
+    if (url && url.startsWith('/api/users')) {
+      usersAPI(req, res);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Non existing endpoint' }));
+    }
+  } catch (err) {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Something went wrong' }));
   }
 });
 
